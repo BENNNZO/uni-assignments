815d4b64beaab979c9e3b7836e7e006bfb8f27ae
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-06-12 00:02:32 -0400

    finished writing main logic

--- cis-210/w2/TablespoonConverter.java
+++ cis-210/w2/TablespoonConverter.java
@@ -2,16 +2,35 @@ package w2;
 
 public class TablespoonConverter {
     public static void main(String[] args) {
-        final int TSP_IN_TBS = 3;
+        final int INITIAL_TEASPOONS = 16;
 
-        final int TEASPOONS = 16;
+        final Measurement RESULT = convertToTablespoons(INITIAL_TEASPOONS);
 
-        final int TABLESPOONS_NEEDED = TEASPOONS / TSP_IN_TBS;
-
-        final int TEASPOONS_LEFTOVER = TEASPOONS % TSP_IN_TBS;
-
-        System.out.println(TEASPOONS);
-        System.out.println(TABLESPOONS_NEEDED);
-        System.out.println(TEASPOONS_LEFTOVER);
+        printFormattedOutput(INITIAL_TEASPOONS, RESULT);
     }
+
+    static Measurement convertToTablespoons(int teaspoons) {
+        final int TEASPOONS_PER_TABLESPOON = 3;
+
+        final int TABLESPOONS_NEEDED = teaspoons / TEASPOONS_PER_TABLESPOON;
+        final int TEASPOONS_LEFTOVER = teaspoons % TEASPOONS_PER_TABLESPOON;
+
+        return new Measurement(TABLESPOONS_NEEDED, TEASPOONS_LEFTOVER);
+    }
+
+    static void printFormattedOutput(int initialTeaspoons, Measurement values) {
+        final String FORMATTED_STRING = String.format("""
+                ----------------INITIAL----------------
+                Teaspoons:    %d
+
+                ---------------CONVERTED---------------
+                Tablespoons:  %d
+                Teaspoons:    %d
+                """, initialTeaspoons, values.tablespoons(), values.leftoverTeaspoons());
+
+        System.out.println(FORMATTED_STRING);
+    }
+}
+
+record Measurement(int tablespoons, int leftoverTeaspoons) {
 }

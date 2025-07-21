6db4c9497390a83eb81dd62ae2295d766d98a9b7
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-07-20 21:26:35 -0400

    added comments to the weather data class

--- cis-210/w7/WeatherData.java
+++ cis-210/w7/WeatherData.java
@@ -6,48 +6,50 @@
 package w7;
 
 public class WeatherData {
-    // CONTRUSTORS AND INIT VARS
+    // Instance variables for temperature and wind speed
     private double dailyTemp;
     private int windSpeed;
 
+    // Default constructor initializes values to 0
     public WeatherData() {
         this.dailyTemp = 0;
         this.windSpeed = 0;
     }
 
+    // Parameterized constructor
     public WeatherData(double dailyTemp, int windSpeed) {
         this.dailyTemp = dailyTemp;
         this.windSpeed = windSpeed;
     }
 
-
-    // GET FUNCTIONS:
+    // Getter for daily temperature
     public double getDailyTemp() {
         return dailyTemp;
     }
 
+    // Getter for wind speed
     public int getWindSpeed() {
         return windSpeed;
     }
 
-
-    // SET FUNCTIONS
+    // Setter for daily temperature
     public void setDailyTemp(double dailyTemp) {
         this.dailyTemp = dailyTemp;
     }
 
+    // Setter for wind speed (only sets if positive)
     public void setWindSpeed(int windSpeed) {
         if (windSpeed > 0) {
             this.windSpeed = windSpeed;
         }
     }
 
-
-    // OTHER FUNCTIONS
+    // Returns a string representation of the object
     public String toString() {
         return "Weather Data: Daily Temperature = " + this.dailyTemp + " F. Wind Speed = " + this.windSpeed + " mph";
     }
 
+    // Checks if two WeatherData objects are equal
     public boolean equals(Object o) {
         if (!(o instanceof WeatherData)) {
             return false;
